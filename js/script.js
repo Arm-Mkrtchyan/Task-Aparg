dccd4c979c111a9cb6a268de267cbc7bab942dfc
Armen Mkrtchyan <arm-mkrtchyan@mail.ru>
2019-01-21 01:05:04 +0400

    success

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
-
 // function for get Flick images with tags
 function getFlickJson(tag) {
 
     // flickr api URL
     var flickApiURL = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-
-    $("#flickr_images").empty();
 
     // JSON for get images
     $.getJSON(flickApiURL, {
@@ -15,7 +12,9 @@ function getFlickJson(tag) {
     }).done(function (data) {
 
         $.each(data.items, function (index, item) {
-            $("<img>").attr('class', 'draggable ' +  tag).attr("src", item.media.m).appendTo("#flickr_images");
+
+            // add 5 pieces image by tag
+            $("<img>").attr('class', 'draggable ' + tag).attr("src", item.media.m).appendTo("#flickr_images");
 
             if (index == 4) {
                 return false;
@@ -26,37 +25,69 @@ function getFlickJson(tag) {
 
 function getImages(tags) {
     var tagsArr = tags.split(' ');
+
+    // clear the old blocks before adding new images
+    $("#flickr_images").empty();
     $(".images_folders").empty();
-    $.each(tagsArr, function(index, value){
+    $(".images_folders_content").empty();
+    $('.success_message').fadeOut().removeClass('active_massage');
+
+    $.each(tagsArr, function (index, value) {
         getFlickJson(tagsArr[index]);
-        $("<div>").attr('class', tagsArr[index]+"folder" + ' dropFolder').attr("data-type", '.'+tagsArr[index]).text(tagsArr[index]).appendTo(".images_folders");
-        // drag('draggble', tagsArr[index]+"folder");
-        $('.' + tagsArr[index]).draggable({containment: ".cont_wrap", revert: true, revertDuration: 1});
-        console.log(tagsArr[index])
+        // generate folders to move
+        $("<div>").attr('class', 'dropFolder').attr("data-type", tagsArr[index]).text(tagsArr[index]).appendTo(".images_folders");
+        // generate folders to keep images
+        $("<div>").attr('class', tagsArr[index] + "folder photos_basket").appendTo(".images_folders_content");
     });
 
     setTimeout(function () {
-        drag('draggable', 'dropFolder')
 
-    }, 1000)
+        // make the images moveable
+        $('.draggable').draggable({
+            revert: "invalid", // when not dropped, the item will revert back to its initial position
+            containment: "document", // images moving area
+            helper: "clone", // moving type
+            start: function (e, ui) {
+                $(this).addClass('draggingImg');
+            },
+            stop: function (e, ui) {
+                $(this).removeClass('draggingImg');
+            },
+            cursor: 'move'
+        });
+
+        // drop images in folders
+        $(".dropFolder").droppable({
+            hoverClass: "hover",
+            activeClass: "active",
+            drop: function (event, ui) {
+                var $item = ui.draggable;
+                var folderType = $(this).attr('data-type');
+
+                // drop, if categories match
+                if ($item.hasClass(folderType)) {
+                    $item.appendTo($('.' + folderType + 'folder'));
+                    $item.removeClass('draggingImg');
+
+                    // appearance of the message
+                    if($('#flickr_images img').length == 1){
+                        $('.success_message').fadeIn().addClass('active_massage');
+                    }
+                }
+            }
+        });
+    }, 1500)
 }
 
+// click for the search button
 $('.get_images_btn').on('click', function () {
     var searchWords = $('#images_tags').val();
     getImages(searchWords);
 });
 
-
-// drag and drop function
-function drag(draggable, droppable) {
-    $('.' + draggable).draggable({containment: ".cont_wrap", revert: true, revertDuration: 1});
-    $("."+droppable).droppable({
-        accept: "."+draggable,
-        activeClass: 'droppable-active',
-        hoverClass: 'hover',
-        drop: function(ev, ui) {
-            $(this).append(ui.draggable);//move the image to the folder
-
-        }
-    });
-}
+//click for images folder
+$(document).on('click', '.dropFolder', function () {
+    var activeFolder = $(this).attr('data-type');
+    $('.photos_basket').hide();
+    $('.'+activeFolder+('folder')).fadeIn();
+});
